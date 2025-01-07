d08d70094f57134ee8582e33f46f95644b16e4c5
PRASAD KARMALKAR <prasadkarmalkar2020@gmail.com>
2025-01-07 13:47:33 +0530

    Deploy dist

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/xora/",
-  build: {
-    outDir: '../build',
-    emptyOutDir: true, // also necessary
-  }
 })
